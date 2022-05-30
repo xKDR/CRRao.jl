62859fc92cc27645573afdf3a39d13abac24a87b
ShouvikGhosh2048 <shouvikg.cal@gmail.com>
2022-05-30 10:49:34 +0530

    Added basic tests

---
+++ test/basic/LogisticRegression.jl
@@ -0,0 +1,23 @@
+turnout = dataset("Zelig", "turnout")[1:100,:] # Take a subset of rows to reduce input size
+
+links = [Logit(), Probit(), Cloglog(), Cauchit()]
+
+priors = [
+    Prior_Ridge(),
+    Prior_Laplace(),
+    Prior_Cauchy(),
+    Prior_TDist(),
+    Prior_Uniform(),
+]
+
+for link in links
+    model = @fitmodel((Vote ~ Age + Race + Income + Educate), turnout, LogisticRegression(), link)
+    @test sizeof(model) > 0
+end
+
+for prior in priors
+    for link in links
+        model = @fitmodel((Vote ~ Age + Race + Income + Educate), turnout, LogisticRegression(), link, prior)
+        @test sizeof(model) > 0
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
-using CRRao
-using Test
+using CRRao, Test, StableRNGs, Logging, RDatasets
+
+Logging.disable_logging(Logging.Warn)
+
+CRRao.setprogress!(false)
+CRRao.set_rng(StableRNG(31))
 
 @testset "CRRao.jl" begin
-    # Write your tests here.
+    @testset "Basic Tests" begin
+        @testset "Linear Regression" begin
+            include("basic/LinearRegression.jl")
+        end
+
+        @testset "Logistic Regression" begin
+            include("basic/LogisticRegression.jl")
+        end
+
+        @testset "Poisson Regression" begin
+            include("basic/PoissonRegression.jl")
+        end
+
+        @testset "Negative Binomial Regression" begin
+            include("basic/NegBinomialRegression.jl")
+        end
+    end
 end
