95f5710e0a84efe5997fd8322b2f686257f88b9c
Siddhant Chaudhary <urssidd@gmail.com>
2022-06-03 12:31:04 +0200

    Added a type to represent all frequentist models.

--- src/fitmodel.jl
+++ src/fitmodel.jl
@@ -1,3 +1,19 @@
+"""
+Type to represent frequentist models returned by `fitmodel` functions. This type is used internally by the package to represent all frequentist regression models.
+"""
+struct FrequentistRegressionModel{RegressionType}
+   model::StatsBase.RegressionModel
+end
+
+"""
+```julia
+FrequentistRegressionModel(::Symbol, ::StatsBase.RegressionModel)
+```
+
+Constructor for `FrequentistRegressionModel`. Used by `fitmodel` functions to return a frequentist regression model.
+"""
+FrequentistRegressionModel(RegressionType::Symbol, model::StatsBase.RegressionModel) = FrequentistRegressionModel{RegressionType}(model)
+
 # Including fitmodel definitions
 
 ## Regression Models
