dd91f44d0b369dcdf2aba18dfaf66a41ba4b3103
Siddhant Chaudhary <urssidd@gmail.com>
2022-06-04 00:25:10 +0200

    Made changes to incorporate `FrequentistRegression` in the code for negative binomial regression.

--- src/fitmodel_defs/negativebinomial_regression.jl
+++ src/fitmodel_defs/negativebinomial_regression.jl
@@ -1,6 +1,38 @@
 # Include NegBinom_Reg definitions
 include("regression_models/NegBinomialRegression.jl")
 
+function negativebinomial_reg(formula::FormulaTerm, data::DataFrame, Link::GLM.Link)
+  formula = apply_schema(formula, schema(formula, data))
+  model = glm(formula, data, NegativeBinomial(), Link)
+  return FrequentistRegression(:NegativeBinomialRegression, model)
+end
+
+"""
+```julia
+fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::NegBinomRegression)
+```
+
+Fit a Negative Binomial Regression model on the input data (with the default link function being the Log link). Uses the `glm` method from the [GLM](https://github.com/JuliaStats/GLM.jl) package under the hood.
+
+# Arguments
+- `formula`: A formula term representing dependencies between the columns in the dataset.
+- `data`: The dataset. 
+- `modelClass`: Object representing the type of regression, which is Negative Binomial Regression in this case.
+
+# Example
+
+```julia-repl
+julia> using CRRao, RDatasets, StatsBase
+
+julia> sanction = dataset("Zelig", "sanction");
+
+julia> container = @fitmodel(Num ~ Target + Coop + NCost, sanction, NegBinomRegression());
+```
+"""
+function fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::NegBinomRegression)
+    return negativebinomial_reg(formula, data, LogLink())
+end
+
 """
 ## NegBinomial Regression with Ridge Prior
 
@@ -205,33 +237,5 @@ end
 """
 function fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::NegBinomRegression,PriorMod::Prior_Uniform,h::Float64=0.1,sim_size::Int64=10000)
     ans = NegBinom_Reg(formula,data,Prior_Uniform(),h,sim_size)
-    ans
-end
-
-"""
-## NegBinomial Regression - Likelihood method
-
-     ## link
-     z = α .+ X * β
-     mu = exp.(z)
- 
-     #likelihood
-     for i = 1:n
-       y[i] ~ NegativeBinomial2(mu[i],λ)
-     end
- 
-    ```Julia
-
-    Julia> using RDatasets
-    Julia> sanction = dataset("Zelig", "sanction");
-    Julia> model = @fitmodel(Num ~ Target + Coop + NCost
-                            , sanction
-                            , NegBinomRegression());
-
-    ```
- 
-"""
-function fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::NegBinomRegression)
-    ans = NegBinom_Reg(formula,data,"LogLink")
     ans
 end
