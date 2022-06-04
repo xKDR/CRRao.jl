077ef840bd5e296888a27687083318ba354ecc5e
Siddhant Chaudhary <urssidd@gmail.com>
2022-06-04 20:15:28 +0200

    Modifying code for poisson regression to incorporate the new struct.

--- src/fitmodel_defs/poisson_regression.jl
+++ src/fitmodel_defs/poisson_regression.jl
@@ -1,34 +1,36 @@
 # Include Poisson_Reg definitions
 include("regression_models/PoissonRegression.jl")
 
-"""
-# Poisson Regression - Likelihood Method
-    
-    ## Link Function
-    z = X*β
-
-    μ = exp.(z)
-
-    ## likelihood
-    for i = 1:n
-        y[i] ~ Poisson(μ[i])
-    end
-    
-   ```Julia
-
-   Julia> sanction = dataset("Zelig", "sanction");
-
-   Julia> model = @fitmodel(Num ~ Target + Coop + NCost
-                           , sanction
-                           , PoissonRegression());
-
-   ```
-"""
-function fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::PoissonRegression)
-   ans = Poisson_Reg(formula,data)
-   ans     
+function poisson_reg(formula::FormulaTerm, data::DataFrame, Link::GLM.Link)
+    formula = apply_schema(formula, schema(formula, data))
+    model = glm(formula, data, Poisson(), Link)
+    return FrequentistRegression(:PoissonRegression, model)
 end
 
+"""
+```julia
+fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::PoissonRegression)
+```
+
+Fit a Poisson Regression model on the input data (with the default link function being the Log link). Uses the `glm` method from the [GLM](https://github.com/JuliaStats/GLM.jl) package under the hood.
+
+# Arguments
+- `formula`: A formula term representing dependencies between the columns in the dataset.
+- `data`: The dataset. 
+- `modelClass`: Object representing the type of regression, which is Poisson Regression in this case.xc
+# Example
+
+```julia-repl
+julia> using CRRao, RDatasets, StatsBase
+
+julia> sanction = dataset("Zelig", "sanction");
+
+julia> container = @fitmodel(Num ~ Target + Coop + NCost, sanction, PoissonRegression());
+```
+"""
+function fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::PoissonRegression)
+    return poisson_reg(formula, data, LogLink())
+end
 
 """
 # Poisson Regression with Ridge Prior using HMC with Turing
