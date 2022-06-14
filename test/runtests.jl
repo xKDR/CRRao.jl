08673a20429988b82f19935483816c18284670a6
ShouvikGhosh2048 <shouvikg.cal@gmail.com>
2022-06-14 14:53:37 +0530

    Restricted tests to numerical linear regression with prior tests + changed the test data

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,23 +6,23 @@ CRRao.setprogress!(false)
 CRRao.set_rng(StableRNG(123))
 
 @testset "CRRao.jl" begin
-    @testset "Basic Tests" begin
-        @testset "Linear Regression" begin
-            include("basic/LinearRegression.jl")
-        end
+    # @testset "Basic Tests" begin
+    #     @testset "Linear Regression" begin
+    #         include("basic/LinearRegression.jl")
+    #     end
 
-        @testset "Logistic Regression" begin
-            include("basic/LogisticRegression.jl")
-        end
+    #     @testset "Logistic Regression" begin
+    #         include("basic/LogisticRegression.jl")
+    #     end
 
-        @testset "Poisson Regression" begin
-            include("basic/PoissonRegression.jl")
-        end
+    #     @testset "Poisson Regression" begin
+    #         include("basic/PoissonRegression.jl")
+    #     end
 
-        @testset "Negative Binomial Regression" begin
-            include("basic/NegBinomialRegression.jl")
-        end
-    end
+    #     @testset "Negative Binomial Regression" begin
+    #         include("basic/NegBinomialRegression.jl")
+    #     end
+    # end
 
     @testset "Numerical Tests" begin
         @testset "Linear Regression" begin
