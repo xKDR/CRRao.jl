7ce26a3f5d7bc83c68832e09446cd68fbdef6d3e
Siddhant Chaudhary <urssidd@gmail.com>
2022-06-21 03:03:00 +0530

    Modifying docstring for frequentist linear regression.

--- src/frequentist/linear_regression.jl
+++ src/frequentist/linear_regression.jl
@@ -3,43 +3,7 @@
 fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::LinearRegression)
 ```
 
-Fit an OLS Linear Regression model on the input data. Uses the `lm` method from the [GLM](https://github.com/JuliaStats/GLM.jl) package under the hood.
-
-# Arguments
-
-- `formula`: A formula term representing dependencies between the columns in the dataset.
-- `data`: The dataset.
-- `modelClass`: Object representing the type of regression, which is Linear Regression in our case.
-
-```julia-repl
-julia> using CRRao, RDatasets, StatsPlots
-
-julia> df = dataset("datasets", "mtcars");
-
-julia> container = @fitmodel(MPG ~ HP + WT + Gear, df, LinearRegression());
-
-julia> coeftable(container);
-
-julia> sigma(container);
-
-julia> loglikelihood(container);
-
-julia> aic(container);
-
-julia> bic(container);
-
-julia> r2(container);
-
-julia> adjr2(container);
-
-julia> predict(container);
-
-julia> residuals(container);
-
-julia> cooksdistance(container);
-
-julia> plot(cooksdistance(container));
-```
+Fit an OLS Linear Regression model on the input data. Uses the `lm` method from the [GLM](https://github.com/JuliaStats/GLM.jl) package under the hood. Returns an object of type `FrequentistRegression{:LinearRegression}`.
 """
 function fitmodel(formula::FormulaTerm, data::DataFrame, modelClass::LinearRegression)
     formula = apply_schema(formula, schema(formula, data))
