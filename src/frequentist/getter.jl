e1bb66ce299130d5aaf7305ba67b041f53bcb382
ShouvikGhosh2048 <shouvikg.cal@gmail.com>
2022-07-07 10:28:08 +0530

    Added predict for logistic regression

--- src/frequentist/getter.jl
+++ src/frequentist/getter.jl
@@ -35,6 +35,32 @@ function predict(container::FrequentistRegression{:LinearRegression}, newdata::D
     y_pred
 end
 
+function predict(container::FrequentistRegression{:LogisticRegression}, newdata::DataFrame)
+    formula = container.formula
+    fm_frame = ModelFrame(formula, newdata)
+    X = modelmatrix(fm_frame)
+    beta = container.beta
+    z = X * beta
+
+    if (container.Link == "LogitLink")
+        p = exp.(z) ./ (1 .+ exp.(z))
+
+    elseif (container.Link == "ProbitLink")
+        p = Probit_Link.(z)
+
+    elseif (container.Link == "CauchitLink")
+        p = Cauchit_Link.(z)
+
+    elseif (container.Link == "CloglogLink")
+        p = Cloglog_Link.(z)
+
+    else
+        println("This link function is not part of logistic regression family.")
+        println("-------------------------------------------------------------")
+    end
+    p
+end
+
 function predict(container::FrequentistRegression{:PoissonRegression}, newdata::DataFrame)
     formula = container.formula
     fm_frame = ModelFrame(formula, newdata)
