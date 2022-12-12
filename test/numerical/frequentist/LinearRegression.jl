2a5f5e4af8deef487dd2f4b1ce84666fa17168c9
ShouvikGhosh2048 <shouvikg.cal@gmail.com>
2022-12-12 14:57:54 +0530

    Add AIC and BIC tests for Linear, Logistic and Poisson models.

--- test/numerical/frequentist/LinearRegression.jl
+++ test/numerical/frequentist/LinearRegression.jl
@@ -1,14 +1,16 @@
 mtcars = dataset("datasets", "mtcars")
 
-formulae = [
-    @formula(MPG ~ HP + WT + Gear),
-    @formula(MPG ~ 0 + HP + WT + Gear),
-    @formula(MPG ~ HP + HP^2 + WT + WT * HP),
-    @formula(log(MPG) ~ log(HP) + log(WT))
+tests = [
+    (@formula(MPG ~ HP + WT + Gear), 157.052778719219475, 164.381458233218098),
+    (@formula(MPG ~ 0 + HP + WT + Gear), 186.905400588726366, 192.768344199925281),
+    (@formula(MPG ~ HP + HP^2 + WT + WT * HP), 145.922971789355415, 154.717387206153774),
+    (@formula(log(MPG) ~ log(HP) + log(WT)), -48.353276320549490, -42.490332709350582)
 ]
 
-for f in formulae
-    crrao_model = fit(f, mtcars, LinearRegression())
-    glm_model = lm(f, mtcars)
+for (test_formula, test_aic, test_bic) in tests
+    crrao_model = fit(test_formula, mtcars, LinearRegression())
+    glm_model = lm(test_formula, mtcars)
     compare_models(crrao_model, glm_model, mtcars)
+    @test isapprox(aic(crrao_model), test_aic)
+    @test isapprox(bic(crrao_model), test_bic)
 end
--- test/numerical/frequentist/PoissonRegression.jl
+++ test/numerical/frequentist/PoissonRegression.jl
@@ -1,14 +1,16 @@
 sanction = dataset("Zelig", "sanction")
 
-formulae = [
-    @formula(Num ~ Target + Coop + NCost),
-    @formula(Num ~ 0 + Target + Coop + NCost),
-    @formula(Num ~ Target + Target^2 + Coop + Coop * Target),
-    @formula(Num ~ log(Target) + log(Coop))
+tests = [
+    (@formula(Num ~ Target + Coop + NCost), 580.673868966946884, 594.814121927084443),
+    (@formula(Num ~ 0 + Target + Coop + NCost), 580.673868966946770, 594.814121927084329),
+    (@formula(Num ~ Target + Target^2 + Coop + Coop * Target), 871.418230125844502, 883.201774259292506),
+    (@formula(Num ~ log(Target) + log(Coop)), 944.326386272138961, 951.396512752207741)
 ]
 
-for f in formulae
-    crrao_model = fit(f, sanction, PoissonRegression())
-    glm_model = glm(f, sanction, Poisson(), LogLink())
+for (test_formula, test_aic, test_bic) in tests
+    crrao_model = fit(test_formula, sanction, PoissonRegression())
+    glm_model = glm(test_formula, sanction, Poisson(), LogLink())
     compare_models(crrao_model, glm_model, sanction)
+    @test isapprox(aic(crrao_model), test_aic)
+    @test isapprox(bic(crrao_model), test_bic)
 end
