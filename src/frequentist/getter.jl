c005635d97dde3ca8114f4af61de4c54359436c7
Mousum <mousum@spotle.ai>
2022-12-12 10:48:04 +0530

    added coef function to extract coefficients from model

--- src/frequentist/getter.jl
+++ src/frequentist/getter.jl
@@ -1,5 +1,31 @@
 """
 ```julia
+coef(container::FrequentistRegression)
+```
+
+Estimated coefficients of the model. Extends the `coef` method from [StatsAPI.jl](https://github.com/JuliaStats/StatsAPI.jl).
+
+# Example
+
+```julia
+using CRRao, RDatasets, StatsModels
+
+# Get the dataset
+mtcars = dataset("datasets", "mtcars")
+
+# Train the model
+container = fit(@formula(MPG ~ HP + WT + Gear), mtcars, LinearRegression())
+
+# Get table of coefficients
+coef(container)
+```
+"""
+function coef(container::FrequentistRegression)
+    return StatsBase.coef(container.model)
+end
+
+"""
+```julia
 coeftable(container::FrequentistRegression)
 ```
 
