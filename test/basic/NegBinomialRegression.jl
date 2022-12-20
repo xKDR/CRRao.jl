9d0d10bf1515261f9b3e562ff264a28f18e2b4ba
Mousum <mousum@spotle.ai>
2022-12-20 22:51:36 +0530

    updated test case

--- test/basic/NegBinomialRegression.jl
+++ test/basic/NegBinomialRegression.jl
@@ -9,11 +9,12 @@ priors = [
 
 CRRao.set_rng(StableRNG(123))
 
-@testset "Frequentist Binomial Regression" begin
+@testset "Frequentist NegativeBinomial Regression" begin
     model = fit(@formula(Num ~ Target + Coop + NCost), sanction, NegBinomRegression())
     @test sizeof(model) > 0
-    @test coef(model) ≈ [ -1.1451746163020158, 0.00862527221332845, 1.0639661414178674, -0.23510970207997078,
-                          1.3076725069242925, 0.18345340856394354]
+    @test coef(model) ≈ [-1.1450754826987306, 0.008593046159347997, 
+                         1.0639242654967949, -0.23501846904848583,
+                         1.307960123972135, 0.18348159198531477]
     @test aic(model) ≈ 344.8795490118083
     @test bic(model) ≈ 361.3765107986354
 end
